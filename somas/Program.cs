45bbd763e54832ef432815e00037da7129a8f5e9
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-23 18:53:56 -0400

    equacoes

---
+++ somas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace somas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor_01 = 2;
+            int valor_02 = 4;
+
+            Console.WriteLine("A soma e igual " + (valor_01 + valor_02));
+            Console.WriteLine("A subitracao e igual " + (valor_01 - valor_02));
+            Console.WriteLine("A divisao e igual a " + (valor_02 / valor_01));
+            Console.WriteLine("A multiplicacao e igual a " + (valor_01 * valor_02));
+        }
+    }
+}
